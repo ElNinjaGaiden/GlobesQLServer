1e6332eaf5098fa6e0537e3280e6ee42e7a199de
Diego García <diego.garcia.segura@gmail.com>
2017-10-13 15:28:49 -0600

    Client url

--- dist/src/config.js
+++ dist/src/config.js
@@ -3,4 +3,4 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-var clientOrigin = exports.clientOrigin = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:3000';
+var clientOrigin = exports.clientOrigin = process.env.NODE_ENV === 'production' ? 'https://globesql.herokuapp.com' : 'http://localhost:3000';
